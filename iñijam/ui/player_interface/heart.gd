699e3cd14b9155d07972228c143df6ae34a74886
JoseArLoLo <92884210+JoseArLoLo@users.noreply.github.com>
2026-08-30 19:38:35 -0700

    Ajustes

--- iñijam/ui/player_interface/heart.gd
+++ iñijam/ui/player_interface/heart.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 class_name Heart
 
 
@@ -9,46 +9,44 @@ const LOST_FRAMES: Array[int] = [3, 4]
 @export var fall_distance: float = 40.0
 @export var fall_duration: float = 0.4
 
+@onready var _alive: Sprite2D = $Alive
+@onready var _empty: Sprite2D = $Empty
+
 var is_alive: bool = true
 
 var _frame_index: int = 0
+var _empty_frame_index: int = 0
 var _elapsed: float = 0.0
-var _is_falling: bool = false
 
 
 func _ready() -> void:
-	frame = ALIVE_FRAMES[0]
+	_alive.frame = ALIVE_FRAMES[0]
 
 
 func _process(delta: float) -> void:
-	if _is_falling:
-		return
 	_elapsed += delta
-	if _elapsed >= frame_duration:
-		_elapsed -= frame_duration
-		_advance_frame()
+	if _elapsed < frame_duration:
+		return
+	_elapsed -= frame_duration
 
-
-func _advance_frame() -> void:
-	var frames: Array[int] = ALIVE_FRAMES if is_alive else LOST_FRAMES
-	_frame_index = (_frame_index + 1) % frames.size()
-	frame = frames[_frame_index]
+	if is_alive:
+		_frame_index = (_frame_index + 1) % ALIVE_FRAMES.size()
+		_alive.frame = ALIVE_FRAMES[_frame_index]
+	elif _empty.visible:
+		_empty_frame_index = (_empty_frame_index + 1) % LOST_FRAMES.size()
+		_empty.frame = LOST_FRAMES[_empty_frame_index]
 
 
 func lose() -> void:
 	if not is_alive:
 		return
 	is_alive = false
-	_is_falling = true
 
-	var fall_tween: Tween = create_tween().set_parallel(true)
-	fall_tween.tween_property(self, "position:y", position.y + fall_distance, fall_duration).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
-	fall_tween.tween_property(self, "modulate:a", 0.0, fall_duration)
-	await fall_tween.finished
+	_empty.frame = LOST_FRAMES[0]
+	_empty.visible = true
 
-	position.y -= fall_distance
-	modulate.a = 1.0
-	_frame_index = 0
-	_elapsed = 0.0
-	frame = LOST_FRAMES[0]
-	_is_falling = false
+	var fall_tween: Tween = create_tween()
+	fall_tween.set_parallel(true)
+	fall_tween.tween_property(_alive, "position:y", _alive.position.y + fall_distance, fall_duration).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	fall_tween.tween_property(_alive, "modulate:a", 0.0, fall_duration)
+	fall_tween.chain().tween_callback(func() -> void: _alive.visible = false)
